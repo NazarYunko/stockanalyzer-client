d9df3a02e54ba9d12b6b18378de1d9b8bfe42ce3
prometej007 <nazar.danilyak46297@gmail.com>
2021-04-17 18:46:33 +0300

    add redirect

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
-import {TickerComponent} from "./ticker/ticker.component";
+import {TickerComponent} from './ticker/ticker.component';
 
 
 const routes: Routes = [
+  {path: '', redirectTo: 'screener', pathMatch: 'full'},
   {path: 'screener', component: TickerComponent}
 ];
 
